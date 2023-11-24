2722f41026127ca62b59617646e4cda6e3b89b5a
18OJohnston.ACC <18ojohnston.acc@lwetb.ie>
2023-11-24 14:36:12 +0000

    b

---
+++ OisinJohnstonQuestion16_A.py
@@ -0,0 +1,7 @@
+# Question 16(a)
+# Name and School: 
+(lambda s_name, f_name, age : print(f"Hello {f_name} {s_name} you are {age} years old \r\n{f_name}, you will receive {'the MRNA' if age > 12 and age < 49 else 'the ADENO' if age >= 50 else 'no'")) (input('Enter your surname: '), input("Enter your first name: "), input("Enter your age: "))
+
+
+
+
